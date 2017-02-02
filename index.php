9746f9fd62e9b0e5fd03eef544322c89d02fd739
zundg@tormail.com <zundg@tormail.com>
2017-02-02 10:07:52 +0100

    updated for php7 (mysql/mysqli, split()) and include config example for fortigate firewalls

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 include("config.php");
-
+$key="";
 $k=explode("?",$_SERVER["REQUEST_URI"]);
-$key=preg_replace('/[^0-9a-zA-Z]*/', '', $k[1]);
-if ($key == "") { # oh shits, fritz!box is too stupid to GET an URL....
+if(isset($k[1])) $key=preg_replace('/[^0-9a-zA-Z]*/', '', $k[1]);
+if (isset($_SERVER["PHP_AUTH_PW"])) { # oh shits, fritz!box is too stupid to GET an URL....
 	$key=$_SERVER["PHP_AUTH_PW"];
 }
 $key=preg_replace('/[^0-9a-zA-Z]*/', '', $key);
@@ -14,20 +14,21 @@ if(strpos($ip, ":") != false) {
 	$type = "A";
 }
 if($key != ""){
-	mysql_query("UPDATE records SET content='$ip', change_date=unix_timestamp(),type='$type' WHERE webkey='$key';");
-	$r=mysql_query("select content from records where type='SOA' and domain_id=(select domain_id from records where webkey='$key')");
-	$row=mysql_fetch_row($r);
+	mysqli_query($db, "UPDATE records SET content='$ip', change_date=unix_timestamp(),type='$type' WHERE webkey='$key';");
+	$r=mysqli_query($db, "select content from records where type='SOA' and domain_id=(select domain_id from records where webkey='$key')");
+	$row=mysqli_fetch_row($r);
 	$soa=explode(" ", $row[0]);
-	$soa[2]++;
-	$r=mysql_query("select domain_id from records where webkey='$key'");
-	$domain_id=mysql_fetch_row($r);
-	mysql_query("update records set content='" . implode(" ", $soa) . "', change_date=unix_timestamp() 
+	$soa[2]++ || error_log("SOA-Probleme fuer Key '$key'");
+	$soa[2] = "0"; # once set don't need to care about SOA anymore...
+	$r=mysqli_query($db, "select domain_id from records where webkey='$key'");
+	$domain_id=mysqli_fetch_row($r);
+	mysqli_query($db, "update records set content='" . implode(" ", $soa) . "', change_date=unix_timestamp() 
 		where type='SOA' and domain_id=$domain_id[0]");
-	echo "all records for key '$key' have been updated<br>";
+	echo "all records for key '$key' have been updated. <br>";
 } else {
 	$a="<select name=domain>";
-	$r=mysql_query("select id, name from domains where dyndns=1");
-	while($row=mysql_fetch_row($r)){
+	$r=mysqli_query($db, "select id, name from domains where dyndns=1");
+	while($row=mysqli_fetch_row($r)){
 		$a.="<option>$row[1]</option>";
 	}
 	$a.="</select>";
--- signup.php
+++ signup.php
@@ -4,8 +4,8 @@ $host=strtolower(preg_replace('/[^0-9a-zA-Z\-]/', '', $_POST['host']));
 $email=strtolower(preg_replace('/[^0-9a-zA-Z\-\+_@\.]/', '', $_POST['email']));
 $key=webkey();
 $domain=strtolower(preg_replace('/[^0-9a-zA-Z\-\.]/', '', $_POST['domain']));
-$r=mysql_query("select id from domains where dyndns=1 and name='$domain'");
-$row=mysql_fetch_row($r);
+$r=mysqli_query($db, "select id from domains where dyndns=1 and name='$domain'");
+$row=mysqli_fetch_row($r);
 $domain_id=$row[0];
 
 if (! $domain_id > 0) {
@@ -18,9 +18,9 @@ if ($host == "") {
 	exit;
 }
 
-$r=mysql_query("select count(*) from records where name='$host.$domain' union select count(*) from domains where name='$host.$domain'");
-$row1=mysql_fetch_row($r);
-$row2=mysql_fetch_row($r);
+$r=mysqli_query($db, "select count(*) from records where name='$host.$domain' union select count(*) from domains where name='$host.$domain'");
+$row1=mysqli_fetch_row($r);
+$row2=mysqli_fetch_row($r);
 if ($row1[0] > 0 || $row2[0] > 0) {
 	echo "already taken, try a different hostname!";
 	exit;
@@ -29,22 +29,22 @@ if (! checkEmail($email)) {
 	echo "bad email, try again!";
 	exit;
 }
-mysql_query("insert into domains (name, type, account) values ('$host.$domain', 'MASTER', 'EXTERN')");
-$r=mysql_query("select id from domains where name='$host.$domain'");
-$row=mysql_fetch_row($r);
+mysqli_query($db, "insert into domains (name, type, account) values ('$host.$domain', 'MASTER', 'EXTERN')");
+$r=mysqli_query($db, "select id from domains where name='$host.$domain'");
+$row=mysqli_fetch_row($r);
 $domain_id=$row[0];
-mysql_query("insert into records (domain_id, name, type, content, ttl, webkey, email) values ($domain_id, '$host.$domain', 'A', '127.0.0.1', 3600, '$key', '$email')");
-mysql_query("insert into records (domain_id, name, type, content, ttl) values ($domain_id, '$host.$domain', 'NS', '$ns1', 3600)");
-mysql_query("insert into records (domain_id, name, type, content, ttl) values ($domain_id, '$host.$domain', 'NS', '$ns2', 3600)");
-mysql_query("insert into records (domain_id, name, type, content, ttl) values ($domain_id, '$host.$domain', 'SOA', 'ns1.$host.$domain. s-dns.geekbox.info. 1 12000 1800 604800 86400', 60)");
-mysql_query("insert into domainmetadata (domain_id, kind, content) values ($domain_id, 'ALLOW-DNSUPDATE-FROM', '0.0.0.0/0')");
-mysql_query("insert into domainmetadata (domain_id, kind, content) values ($domain_id, 'TSIG-ALLOW-DNSUPDATE', '$host.$domain')");
+mysqli_query($db, "insert into records (domain_id, name, type, content, ttl, webkey, email) values ($domain_id, '$host.$domain', 'A', '127.0.0.1', 3600, '$key', '$email')");
+mysqli_query($db, "insert into records (domain_id, name, type, content, ttl) values ($domain_id, '$host.$domain', 'NS', '$ns1', 3600)");
+mysqli_query($db, "insert into records (domain_id, name, type, content, ttl) values ($domain_id, '$host.$domain', 'NS', '$ns2', 3600)");
+mysqli_query($db, "insert into records (domain_id, name, type, content, ttl) values ($domain_id, '$host.$domain', 'SOA', 'ns1.$host.$domain. s-dns.geekbox.info. 1 12000 1800 604800 86400', 60)");
+mysqli_query($db, "insert into domainmetadata (domain_id, kind, content) values ($domain_id, 'ALLOW-DNSUPDATE-FROM', '0.0.0.0/0')");
+mysqli_query($db, "insert into domainmetadata (domain_id, kind, content) values ($domain_id, 'TSIG-ALLOW-DNSUPDATE', '$host.$domain')");
 `rm /tmp/K$host.$domain*`;
 $k = chop (`/usr/sbin/dnssec-keygen -a hmac-md5 -b128 -K /tmp/ -n USER $host.$domain`);
 $tsig = `/bin/grep Key: /tmp/$k.private`;
 `/bin/rm /tmp/$k.private`;
 $tsig =substr( $tsig, 5, -1);
-mysql_query("insert into tsigkeys (name, algorithm, secret) values ('$host.$domain', 'hmac-md5', '$tsig')");
+mysqli_query($db, "insert into tsigkeys (name, algorithm, secret) values ('$host.$domain', 'hmac-md5', '$tsig')");
 if (mail($email, "$domain dynamic dns service", "Hi, \n
 \n
 Your update-url for $host.$domain is: ".curPageURL()."?$key\n
@@ -56,7 +56,21 @@ Port: 53\n
 Key name: $host.$domain\n
 Key: $tsig\n
 Type: tsig/hmac-md5\n
-\n
+
+Here is a FortiGate config snippet:\n
+config system ddns
+    edit 0
+        set ddns-server genericDDNS
+        set ddns-domain \"$host.$domain\"
+        set monitor-interface \"wan2\"
+        set ddns-server-ip $serverip
+        set ddns-zone \"$host.$domain\"
+        set ddns-auth tsig
+        set ddns-keyname \"$host.$domain\" 
+        set ddns-key \"$tsig\"
+    next
+end
+
 If you did not request this email, please ignore. There will be no further mailings")) {
 	echo "success. check your email!";
 } else {
@@ -73,7 +87,7 @@ function webkey($length = 16) {
 }
 function checkEmail($email) {
   if(preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)){
-    list($username,$domain)=split('@',$email);
+    list($username,$domain)=preg_split('/\@/',$email);
     if(!checkdnsrr($domain,'MX')) { // as if an A record would not be okay, too... 
       return false;
     }
